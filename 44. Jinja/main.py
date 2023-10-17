423b03450d52c6968fb9f0c264f9018fb7af2195
Strazdin_Dmitry <dmitrijstrazdin5@gmail.com>
2023-10-17 18:29:16 +0300

    Added done task

---
+++ 44. Jinja/main.py
@@ -0,0 +1,10 @@
+# from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
+
+file_loader = FileSystemLoader('templates')
+env = Environment(loader=file_loader)
+ 
+tm = env.get_template('page.html')
+msg = tm.render()
+ 
+print(msg)
